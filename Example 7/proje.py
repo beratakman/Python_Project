4e7f685fae6aa684c28c9bab950b2cd903ba24f9
beratakman <berat.akman@guclunet.com>
2024-02-27 23:22:16 +0300

    denemeler bitti

--- Example 7/proje.py
+++ Example 7/proje.py
@@ -12,13 +12,24 @@ userYear0 = int(input("Lütfen Doğduğunuz Yılı Girin. (Sadece Yıl):\n"))
 userMonth0 =  int(input("Lütfen Doğduğunuz Ayı Girin. (Sadece Ay):\n"))
 userDay0 =  int(input("Lütfen Doğduğunuz Günü Girin. (Sadece Gün):\n"))
 
-userTime = date(userYear0, userMonth0, userDay0)
-dayİnBetween = currentTime - userTime
-dayİnBetween = dayİnBetween.days # days == Datetime
 
 
-userYear, userDay = divmod(dayİnBetween, 365)
-userMonth, userDay = divmod(userDay, 30)
+birhday = date(userYear0, userMonth0, userDay0)
+dateBetween = currentTime - birhday
+dayInBetween = dateBetween.days # days == Datetime
+
+
+ 
+
+userYear, userDayMonth = divmod(dayInBetween, 365) #ilk kısım yıl, kalan kısmım günleri(ay bulunmadı)
+userMonth, userDay = divmod(userDayMonth, 30) #ilk kısım ay, kalan kısım günleri
+
+nextAgeMonth, nextAgeDay = divmod(365 - userDayMonth, 30)
+
+print(f"Siz doğalı {userYear} yıl {userMonth} ay {userDay} gün olmuş ve yaklaşık {nextAgeMonth} ay, {nextAgeDay} gün sonra {userYear+1} yaşına gireceksiniz.")
+
+
+exit()
 
 # userYear --> Yıl
 # userMonth --> Ay
@@ -35,7 +46,7 @@ releaseDate0 = date(1, 1, 1)
 releaseDate = date(2, 1, 1)
 
 if userMonth == 0:
-    userMonth000 = userMonth + 1
+    userMonth000 = userMonth
     
     userAge0 = date(userYear, userMonth000, userDay)
     userAge0 = userAge0.day
@@ -47,10 +58,8 @@ if userMonth == 0:
     
     currentTime000 = currentTime.day
     
-    timeİnBetween = oneYearLater - currentTime000
-    
-
-    
+    timeInBetween = oneYearLater - currentTime000
+        
 else:
     userAge = date(userYear, userMonth, userDay)
     userAge = userAge.day
@@ -62,12 +71,12 @@ else:
     
     currentTime000 = currentTime.day
     
-    timeİnBetween = oneYearLater - currentTime000
+    timeInBetween = oneYearLater - currentTime000
     
 
 currentTime0 = currentTime.year
 
-userLastMonth0, userLastDay0 = divmod(timeİnBetween, 30)
+userLastMonth0, userLastDay0 = divmod(timeInBetween, 30)
 
 userLastMonth = userLastMonth0 - userMonth
 userLastDay = userLastDay0 - userDay
